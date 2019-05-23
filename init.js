8b32e0410a3ac23585da8966d3ab0150698811ae
mightytyphoon <laurentboukhadcha@gmail.com>
2019-05-23 18:18:01 -0300

    added init script blank

---
+++ init.js
@@ -0,0 +1,25 @@
+// init a ng desktop project
+// create angular project
+// add fs, nw to global in angular interface
+// init package with name, scope, index
+// add the build scripts, distro scripts etc...
+
+console.log('init a desktop app');
+
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("What is your name ? ", function(name) {
+    rl.question("Where do you live ? ", function(country) {
+        console.log(`${name}, is a citizen of ${country}`);
+        rl.close();
+    });
+});
+
+rl.on("close", function() {
+    console.log("\nBYE BYE !!!");
+    process.exit(0);
+});
